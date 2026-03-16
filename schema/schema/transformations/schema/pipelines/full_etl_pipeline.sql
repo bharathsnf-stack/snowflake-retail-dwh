fd7e442f72ad2c37003aff6c68d60cc5c387226c
bharathsnf-stack <bharathsnf@gmail.com>
2026-03-16 18:51:33 +0530

    Create full_etl_pipeline.sql
    Added ETL pipeline

---
+++ schema/schema/transformations/schema/pipelines/full_etl_pipeline.sql
@@ -0,0 +1,18 @@
+INSERT INTO MART.DIM_CUSTOMER
+SELECT * FROM STAGING.STG_CUSTOMERS;
+
+INSERT INTO MART.DIM_PRODUCT
+SELECT * FROM STAGING.STG_PRODUCTS;
+
+INSERT INTO MART.FACT_SALES
+SELECT
+c.customer_sk,
+p.product_sk,
+o.order_date,
+o.quantity,
+o.quantity * p.price
+FROM STAGING.STG_ORDERS o
+JOIN MART.DIM_CUSTOMER c
+ON o.customer_id = c.customer_id
+JOIN MART.DIM_PRODUCT p
+ON o.product_id = p.product_id;
